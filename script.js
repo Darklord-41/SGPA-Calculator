7b956c43e0f16fdf7093a0cc8ecc7e726b22ec7d
Darklord-41 <kmrinal888@gmail.com>
2025-01-20 11:32:16 +0530

    second commit

--- script.js
+++ script.js
@@ -29,32 +29,77 @@ function addSubject(){
     document.getElementById("grade").value="";
 }
 
-if(document.getElementById("grade")==document.getElementById("A+")){
-    var gradep=10;
-}
-else if(document.getElementById("grade")==document.getElementById("A")){
-    var gradep=9;
-}
-else if(document.getElementById("grade")==document.getElementById("B+")){
-    var gradep=8;
-}
-else if(document.getElementById("grade")==document.getElementById("B")){
-    var gradep=7;
-}
-else if(document.getElementById("grade")==document.getElementById("C+")){
-    var gradep=6;
-}
-else if(document.getElementById("grade")==document.getElementById("C")){
-    var gradep=5;
-}
-else if(document.getElementById("grade")==document.getElementById("D")){
-    var gradep=4;
-}
-else{
-    var gradep=0;
+function giveResult(){
+    function getgradep(grade){
+        let gradep;
+        if(grade=="A+"){
+            gradep=10;
+        }
+        else if(grade=="A"){
+            gradep=9;
+        }
+        else if(grade=="B+"){
+            gradep=8;
+        }
+        else if(grade=="B"){
+            gradep=7;
+        }
+        else if(grade=="C+"){
+            gradep=6;
+        }
+        else if(grade=="C"){
+            gradep=5;
+        }
+        else if(grade=="D"){
+            gradep=4;
+        }   
+        else{
+            gradep=0;
+        }
+        return gradep;
+    }
+    
+    let rows = document.getElementsByTagName("tr");
+    const crdarr = [];
+    const grdarr = [];
+
+
+    for(let i = 1; i < rows.length; i++){
+        let credit = parseInt(rows[i].cells[1].textContent.trim())
+        crdarr.push(credit)
+
+
+        var gradecell = rows[i].cells[2].textContent.trim();
+        var gradepoint = getgradep(gradecell);
+        grdarr.push(gradepoint);
+    }
+    
+    const pointpro = [];
+    for(let i = 0; i < crdarr.length; i++){
+        pointpro.push(crdarr[i]*grdarr[i]);
+    }
+    
+    function arrsum(arr) {
+        let total = 0;
+        for (let i = 0; i < arr.length; i++) {
+          total += arr[i];
+        }
+        return total;
+    }
+    let crsum = arrsum(crdarr);
+    let prosum = arrsum(pointpro);
+    
+    let result = prosum/crsum;
+    document.getElementById("result").innerHTML = "Your SGPA is " + result.toFixed(2);
 }
 
-const crdarr = [];
-for(let i = 1; i < rows.length; i++){
-    crdarr.push(rows[i].textContent)
+function clearResult(){
+    let rows = document.getElementsByTagName("tr");
+
+    var rowCount = rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }  
+    
+    document.getElementById("result").textContent="";
 }
